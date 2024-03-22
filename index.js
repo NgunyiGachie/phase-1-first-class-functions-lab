ff78c51544989505d7cd8ca299d1e963503b8d5a
NgunyiGachie <antogachie@gmail.com>
2024-03-22 07:54:56 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(allDrivers) {
+    return allDrivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(allDrivers) {
+    return allDrivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function (value) {
+        return value * multiplier;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(allDrivers, fn) {
+    return fn(allDrivers);
+}
+
+
+module.exports = {
+    drivers,
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+    selectingDrivers,
+    createFareMultiplier,
+    fareDoubler,
+    fareTripler,
+    selectDifferentDrivers
+};
+
